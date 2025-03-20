6151253cdafbcaba5fc95edbf2f2fd9bb1ac457f
Matt Taylor <matt@matthewjosephtaylor.com>
2025-03-20 14:12:34 -0500

    webclient operations

--- src/WebclientConnectionMap.ts
+++ src/WebclientConnectionMap.ts
@@ -1,24 +1,31 @@
 export type WebClientScrapeResult = {
   url: string;
-  html: string;
-  text?: string;
+  html?: string;
+  markdown?: string;
   pdf?: string;
   screenshot?: string;
   error?: string;
 };
+
+export type WebClientOpName =
+  | "open"
+  | "screenshot"
+  | "pdf"
+  | "html"
+  | "markdown"
+  | "close";
+
+export type WebClientOp = [WebClientOpName, string] | [WebClientOpName];
+
+export type WebClientProgram = WebClientOp[];
+
 export type WebclientConnectionMap = {
-  "webclient.scrape": {
+  "webclient.exec": {
     request: {
       options?: Partial<{}>;
-      body: {
-        url: string | string[];
-        screenshot?: boolean;
-        pdf?: boolean;
-        rateLimitMillis?: number;
-        maxPages?: number;
-      };
+      body: WebClientProgram;
     };
-    response: {};
+    response: WebClientScrapeResult;
     headers: {
       url?: string;
       authToken?: string;
